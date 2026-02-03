3e86e76f272c7b731a4b386315f799461e87b6ed
tanayisiksal <t.tanayisiksal@gmail.com>
2026-02-03 14:19:41 +0300

    Render fetch added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -20,7 +20,7 @@ function App() {
     setIsTyping(true);
   
     try {
-      const response = await fetch("http://localhost:3000/chat", {
+      const response = await fetch("https://hotel-chatbot-3ekv.onrender.com/chat", {
         method: "POST",
         headers: {
           "Content-Type": "application/json"
